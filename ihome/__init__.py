cd1742b800fa0693c4f76c4598ae65bb8b538d78
PetersonZhao <604496499@qq.com>
2018-01-17 22:12:51 +0800

    拆分启动文件，创建项目结构与目录

---
+++ ihome/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import redis
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
+
+from config import config_dict
+from flask import Flask
+
+# 为了方便其他模块调用db，将其放在create外面，延迟初始化
+db = SQLAlchemy()
+# 构建redis链接对象
+redis_store = None
+
+# 为flask补充csrf防护机制
+csrf = CSRFProtect()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    conf = config_dict[config_name]
+
+    app.config.from_object(conf)
+
+    # 延迟初始化数据库
+    db.init_app(app)
+    csrf.init_app(app)
+
+    # 将flask里的session数据保存到redis中
+    Session(app)
+
+    global redis_store
+    redis_store = redis.StrictRedis(host=conf.REDIS_HOST, port=conf.REDIS_PORT)
+
+    return app
--- manage.py
+++ manage.py
@@ -1,47 +1,27 @@
 # -*- coding: utf-8 -*-
-import redis
-from flask import Flask
-from flask_session import Session
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import CSRFProtect
+# import redis
+# from flask import Flask
+# from flask_session import Session
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_wtf import CSRFProtect
+from flask_script import Manager
+from flask_migrate import MigrateCommand, Migrate
+from ihome import create_app, db
 
-app = Flask(__name__)
+app = create_app("develop")
 
+# db = SQLAlchemy(app)
 
-class Config(object):
-    """工程的配置信息"""
-    DEBUG = True
+# 创建管理工具对象
+manager = Manager(app)
+Migrate(app, db)
+manager.add_command("db", MigrateCommand)
 
-    # 数据库MySQL的配置信息
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/ihome"
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-    # 配置redis
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-
-    # flask_session用到的配置信息
-    # 指明保存到redis中
-    SESSION_TYPE = "redis"
-    # 让cookie中的session_id被加密签名处理
-    SESSION_USE_SIGNER = True
-    # 使用的redis实例
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
-    # session有效期，单位秒
-    PERMANENT_SESSION_LIFETIME = 86400
-
-
-app.config.from_object(Config)
-
-db = SQLAlchemy(app)
-
-redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-
+# redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 # 为flask补充csrf防护机制
-csrf = CSRFProtect(app)
-
+# csrf = CSRFProtect(app)
 # 将flask里的session数据保存到redis中
-Session(app)
+# Session(app)
 
 
 @app.route("/")
